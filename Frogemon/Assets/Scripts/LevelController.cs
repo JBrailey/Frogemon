9f905c8cdcc875a39d6d1ab942f2d67e11564c16
Timothy <timothy.ayres@students.aie.edu.au>
2016-07-31 23:50:06 +1000

    Added basic title screen. Added basic game over management.

--- Frogemon/Assets/Scripts/LevelController.cs
+++ Frogemon/Assets/Scripts/LevelController.cs
@@ -4,22 +4,32 @@ using System.Collections;
 public class LevelController : MonoBehaviour
 {
     public GameObject gridObject; // the grid controller object
+    public GameObject titleObject; // the game title
     public int level; // the current level number
 
     CameraController camControl; // the camera controller
     GameObject currentLevelGrid; // the current level grid controller
     bool waitToStart = true;
 
+    GameObject titleInstance; // the title / logo instance
+
+    const float HUD_Z = -2f; // the z depth of HUD elements
+
     // Use this for initialization
     void Start ()
     {
+        // create the title instance and parent it to the camera
+        // note: centered for now, final position to be determined
+        Vector3 titlePos = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, HUD_Z);
+        titleInstance = (GameObject)Instantiate(titleObject, titlePos, Quaternion.identity);
+        titleInstance.transform.parent = Camera.main.transform;
+
         // get the camera controller and position at the top
         camControl = Camera.main.GetComponent<CameraController>();
         camControl.SnapToTop();
 
         // spawn the first level
-        currentLevelGrid = (GameObject)Instantiate(gridObject, new Vector3(0f, 0f, 0f), Quaternion.identity);
-        currentLevelGrid.GetComponent<GridController>().levelController = gameObject;
+        BuildLevel();
 	}
 
 	// Update is called once per frame
@@ -34,6 +44,9 @@ public class LevelController : MonoBehaviour
                 // stop waiting
                 waitToStart = false;
 
+                // hide the title
+                TitleVisible(false);
+
                 // TODO: implement camera pan to start
 
                 // start the level
@@ -43,18 +56,34 @@ public class LevelController : MonoBehaviour
         }
     }
 
+    // show or hide the title
+    void TitleVisible(bool a_visible)
+    {
+        titleInstance.GetComponent<Renderer>().enabled = a_visible;
+    }
+
+    // spawns a level
+    void BuildLevel()
+    {
+        // destroy the current level, if it exists
+        if (currentLevelGrid != null)
+        {
+            DestroyObject(currentLevelGrid);
+        }
+
+        // spawn the level
+        currentLevelGrid = (GameObject)Instantiate(gridObject, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        currentLevelGrid.GetComponent<GridController>().levelController = gameObject;
+    }
+
     // called when the current level has been completed
     public void NextLevel()
     {
-        // destroy the current level
-        DestroyObject(currentLevelGrid);
-
         // increment the level
         ++level;
 
-        // spawn the next level
-        currentLevelGrid = (GameObject)Instantiate(gridObject, new Vector3(0f, 0f, 0f), Quaternion.identity);
-        currentLevelGrid.GetComponent<GridController>().levelController = gameObject;
+        // spawn the new level
+        BuildLevel();
 
         // start the level immediately
         currentLevelGrid.GetComponent<GridController>().autoLevelStart = true;
@@ -64,6 +93,22 @@ public class LevelController : MonoBehaviour
     // called when pikachu died
     public void GameOver()
     {
-        Debug.Log("game over");
+        // TODO: add code to show a game over notice and move this code to a coroutine
+        //       to time-out the game over notice
+
+        // spawn a new level
+        BuildLevel();
+
+        // reset the level number
+        level = 1;
+
+        // show the title
+        TitleVisible(true);
+
+        // snap camera to the top
+        camControl.SnapToTop();
+
+        // wait to start the level
+        waitToStart = true;
     }
 }
