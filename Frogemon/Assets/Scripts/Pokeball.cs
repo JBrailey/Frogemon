9e13f9fd364aef611f3917d9686d90123292e922
ShaneAdamsAIE <sadams45@hotmail.com>
2016-07-31 17:11:20 +1000

    Updated Pokeball and Trainer
    Made some additions.

--- Frogemon/Assets/Scripts/Pokeball.cs
+++ Frogemon/Assets/Scripts/Pokeball.cs
@@ -1,15 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pokeball : MonoBehaviour {
+public class Pokeball : MonoBehaviour
+{
+    float speed = 1f;
+    bool movingRight = true;
+    Vector3 position;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GridController gridController;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = 1f + ((gridController.level * 0.1f) - 0.1f);
+
+        // Check for movement here
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingRight == true)
+        {
+
+        }
+    }
 }
