54ae3f10f0eed3425dfba00d151e16c63dbeefcb
ShaneAdamsAIE <sadams45@hotmail.com>
2016-08-05 21:08:45 +1000

    Got trainer script working for all Trainer prefabs. Removed extra trainer scripts

--- Frogemon/Assets/Scripts/Trainer.cs
+++ Frogemon/Assets/Scripts/Trainer.cs
@@ -5,11 +5,17 @@ public class Trainer : MonoBehaviour {
 
 
     public GameObject pokeBall;
+    public float speedMod;
+    public float intervalMod;
+    public bool doesBurst;
     Animator anim;
 
-    int timerRun = 999; // The timer that runs
-    int timerSet;  // The set value of the timer
+    float timerRun = 999; // The timer that runs
+    float timerBurst = 999;
+    float timerSet;  // The set value of the timer
+    float burstSet;
     float speed;
+    bool ballThrown; // Checks whether the first ball has been thrown when Burst is activated
 //    Vector3 position;
     public Vector3 endPosition;
     public int level;
@@ -18,7 +24,13 @@ public class Trainer : MonoBehaviour {
     void Start ()
     {
         anim = GetComponent<Animator>();
-        timerSet = 180 - 2*level;
+        timerSet = (180*intervalMod) - 2*level;
+        if (doesBurst == true)
+        {
+            ballThrown = false;
+            burstSet = ((180 * intervalMod) - 2 * level)+30;
+            timerBurst = burstSet;
+        }
 
         //  Randomise the initial throw
         Random.seed = Random.Range(0, 99999);
@@ -31,17 +43,19 @@ public class Trainer : MonoBehaviour {
     {
         if (timerRun <= 0 )
         {
-            speed = 1f + ((level * 0.1f) - 0.1f);
-            GameObject go = (GameObject)Instantiate(pokeBall,transform.position,Quaternion.identity);
+            speed = (1f * speedMod) + ((level * 0.1f) - 0.1f);
+            GameObject go = (GameObject)Instantiate(pokeBall, transform.position, Quaternion.identity);
             go.transform.parent = transform; // make sure the pokeballs are parented to the trainer
                                              // this will be important for continuous level looping
+
             Pokeball pb = go.GetComponent<Pokeball>();
             pb.speed = speed;
             pb.endPosition = endPosition;
             pb.spin = GetComponent<SpriteRenderer>().flipX ? 1f : -1f;
             timerRun = timerSet;
-            anim.Play("Throw");
+            anim.Play("Throw");          
             StartCoroutine(Wait());
+                
         }
         else
         {
